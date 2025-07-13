4b22d5683903245d573c11fa08d157cf0008d16b
desmonddam <desmonddam64@gmail.com>
2025-07-13 14:25:38 -0600

    added env file for version management

--- src/constants/BaseUrl.js
+++ src/constants/BaseUrl.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://dev.markettoll.com/api/v1/";
 // const BASE_URL="https://backend.markettoll.com/api/v1";
-const BASE_URL = `http://localhost:4001/api/v1`;
-
+// const BASE_URL = `http://localhost:4001/api/v1`;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default BASE_URL;
