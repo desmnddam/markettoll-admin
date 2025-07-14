6843197908a5d3415674fc22fcc0986609913a82
desmonddam <desmonddam64@gmail.com>
2025-07-13 18:34:15 -0600

    update vite config file.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import csslogger from 'vite-postcss-tools';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), csslogger()],
+  plugins: [react()],
 })
